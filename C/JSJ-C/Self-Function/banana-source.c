69dc475b19729b4b8257b5874fbaa9980559bd98
FreezeMandu <sewolminsuk03@gmail.com>
2025-09-04 10:30:49 +0900

    2

---
+++ C/JSJ-C/Self-Function/banana-source.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+//배열예제
+//int main(void) {
+//	int i;
+//	double age[3], to = 0, avg;
+//
+//	printf("3명의 나이 입력: ");
+//	for (i = 0; i < 3; i++) {
+//		scanf_s("%lf", &age[i]);
+//		to += age[i];
+//	}
+//
+//	avg = to / 3.0;
+//	printf("합계: %.1lf\n", to);
+//	printf("3명 나이 평균: %.1lf", avg);
+//}
+
+//input, output, avg 함수 선언! main에서는 초기 변수와 실행 담당
+
+int main(void) {
+	int num[3];
+	int to = 0;
+	double avg;
+
+	Input(num[3]);
+}
+
+void Input(num[3]) {
+
+}
